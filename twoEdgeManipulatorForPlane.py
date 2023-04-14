7a97cbbc55f23ce02ae3ac0ea0cf1f004044a948
Nikita <nik.lizard.mobile@gmail.com>
2023-04-14 11:31:06 +0400

    Add check for unreachable points

--- twoEdgeManipulatorForPlane.py
+++ twoEdgeManipulatorForPlane.py
@@ -3,8 +3,9 @@ class TwoEdgeManipulatorForPlane:
     def __init__(self,r1,r2):
         self.r1 = r1
         self.r2 = r2
-    def tryToReach(self,destX,destY):
-        #x2 = r1 cos(φ1) + r2 cos(φ1 + φ2), y2 = r1 sin(φ1) + r2 sin(φ1 + φ2)
+    def findAngles(self,destX,destY):
+        if (destX ** 2 + destY ** 2) ** 0.5 > self.r1 + self.r2:
+            return []
         φ1,φ2 = sy.symbols('φ1, φ2')
         equationX = self.r1 * sy.cos(φ1) + self.r2 * sy.cos(φ1 + φ2) - destX
         equationY = self.r1 * sy.sin(φ1) + self.r2 * sy.sin(φ1 + φ2) - destY
