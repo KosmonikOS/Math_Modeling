3cdde768dbf35818b9fa6b05cda40c6ea5d423d9
Lana <lana100705@icloud.com>
2023-04-17 15:12:02 +0400

    Fix for angles equal to complex numbers and fix library incompatibility

--- threeEdgeManipulatorForSpace.py
+++ threeEdgeManipulatorForSpace.py
@@ -15,9 +15,9 @@ class ThreeEdgeManipulatorForSpace:
         equation2 = self.r2 * np.cos(φ1) * sy.sin(φ2) + self.r3 * np.cos(φ1) * sy.sin(φ2 + φ3) - destZ * np.cos(φ1)
         result = sy.solve((equation1,equation2),(φ2,φ3))
         if len(result) == 0: return []
-        return [φ1,float(result[0][0]),float(result[1][0])]
+        return [φ1, result[0][0], result[1][0]]
     def findPosition(self, φ1, φ2, φ3):
-        x3 = (self.r1 + self.r2*np.cos(φ2) + self.r3*np.cos(φ2 + φ3))*np.cos(φ1)
-        y3 = (self.r1 + self.r2*np.cos(φ2) + self.r3*np.cos(φ2 + φ3))*np.sin(φ1)
-        z3 = self.r2*np.sin(φ2) + self.r3*np.sin(φ2 + φ3)
+        x3 = (self.r1 + self.r2*sy.cos(φ2) + self.r3*sy.cos(φ2 + φ3))*sy.cos(φ1)
+        y3 = (self.r1 + self.r2*sy.cos(φ2) + self.r3*sy.cos(φ2 + φ3))*sy.sin(φ1)
+        z3 = self.r2*sy.sin(φ2) + self.r3*sy.sin(φ2 + φ3)
         return np.array([x3, y3, z3])
