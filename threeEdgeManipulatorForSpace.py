a0d2d2ad5185d94f38ba4a74863974baa9170a9f
Nikita <nik.lizard.mobile@gmail.com>
2023-04-17 23:19:48 +0400

    Change algorithm of computing 3L in space

--- threeEdgeManipulatorForSpace.py
+++ threeEdgeManipulatorForSpace.py
@@ -2,25 +2,38 @@ import numpy as np
 import sympy as sy
 class ThreeEdgeManipulatorForSpace:
     def __init__(self,r1,r2,r3):
-        self.r1 = r1
-        self.r2 = r2
-        self.r3 = r3
+        self.L12 = r1
+        self.L23 = r2
+        self.L34 = r3
     def findAngles(self,destX,destY,destZ):
-        if np.sqrt(destX ** 2 + destY ** 2 + destZ ** 2) > (self.r1 + self.r2 + self.r3):
+        if np.sqrt(destX ** 2 + destY ** 2 + destZ ** 2) > (self.L12 + self.L23 + self.L34):
             print('End-effecter is outside the workspace')
             return []
         if destY != 0:
             φ1 = np.arctan(destX / destY)
         else:
             φ1 = 0 if destX > 0 else np.pi
-        φ2,φ3 = sy.symbols('φ2, φ3')
-        equation1 = self.r2 * np.cos(φ1) * sy.cos(φ2) + self.r3 * np.cos(φ1) * sy.cos(φ2 + φ3) - (destX - self.r1 * np.cos(φ1))
-        equation2 = self.r2 * np.cos(φ1) * sy.sin(φ2) + self.r3 * np.cos(φ1) * sy.sin(φ2 + φ3) - destZ * np.cos(φ1)
-        result = sy.solve((equation1,equation2),(φ2,φ3))
-        if len(result) == 0: return []
-        return [φ1, result[0][0], result[1][0]]
+        x2 = self.L12 * np.cos(φ1)
+        y2 = self.L12 * np.sin(φ1)
+        C = np.sqrt((destX-x2) ** 2 + (destY-y2) ** 2 + (destZ) ** 2)
+
+        if (self.L23 + self.L34) < C or np.abs(self.L23 - self.L34) > C:
+            print('End-effecter is outside the workspace')
+            return []
+            
+        a = np.rad2deg(np.arccos((self.L23 ** 2 + self.L34 ** 2 - C ** 2) / (2 * self.L23 * self.L34)))
+        b = np.rad2deg(np.arccos((self.L23 ** 2 + C ** 2 - self.L34 ** 2) / (2 * self.L23 * C)))
+
+        
+        J1a = np.rad2deg(np.arctan2(destZ, np.sqrt((destX-x2)**2+(destY-y2)**2))) - b
+        J2a = 180 - a
+        
+
+        J1b = np.rad2deg(np.arctan2(destZ, np.sqrt((destX-x2)**2+(destY-y2)**2))) + b
+        J2b = -(180 - a)
+        return [[φ1,J1a,J2a],[φ1,J1b,J2b]]
     def findPosition(self, φ1, φ2, φ3):
-        x3 = (self.r1 + self.r2*sy.cos(φ2) + self.r3*sy.cos(φ2 + φ3))*sy.cos(φ1)
-        y3 = (self.r1 + self.r2*sy.cos(φ2) + self.r3*sy.cos(φ2 + φ3))*sy.sin(φ1)
-        z3 = self.r2*sy.sin(φ2) + self.r3*sy.sin(φ2 + φ3)
+        x3 = (self.L12 + self.L23*sy.cos(φ2) + self.L34*sy.cos(φ2 + φ3))*sy.cos(φ1)
+        y3 = (self.L12 + self.L23*sy.cos(φ2) + self.L34*sy.cos(φ2 + φ3))*sy.sin(φ1)
+        z3 = self.L23*sy.sin(φ2) + self.L34*sy.sin(φ2 + φ3)
         return np.array([x3, y3, z3])
